21d6af17266db75470e9928dd96d284caf06b0df
Sten Levasseur <sten.levasseur@lonestone.io>
2024-10-01 17:38:12 +0200

    feat: add cors package

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,3 +1,4 @@
+import cors from 'cors'
 import express from 'express'
 import adRoutes from './routes/ad.routes'
 import categoryRoutes from './routes/category.routes'
@@ -6,7 +7,14 @@ import tagRoutes from './routes/tag.routes'
 export function initializeApp() {
   const app = express()
 
+  // // Configuration de CORS pour autoriser uniquement https://example.com
+  // const corsOptions = {
+  //   origin: 'https://example.com',  // Autoriser uniquement cette URL
+  //   optionsSuccessStatus: 200       // Pour assurer la compatibilité avec les anciens navigateurs
+  // }
+
   // Middlewares
+  app.use(cors())
   app.use(express.json())
 
   // Setup routes
